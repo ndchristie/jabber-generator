c35b65106f15e301ad058c6fb0542156cc13b22d
Nathan Christie <ndchristie@gmail.com>
2018-01-22 10:58:26 -0500

    feat: Add additional default filters to reject difficult sounds

--- dist/default-filters.js
+++ dist/default-filters.js
@@ -4,8 +4,19 @@ var no_consonant_pileup_1 = require("./filters/no-consonant-pileup");
 var consistent_glyphs_1 = require("./filters/consistent-glyphs");
 var reject_matches_1 = require("./filters/reject-matches");
 exports.default = [
+    function (candidate, _a) {
+        var _b = _a.prefix, prefix = _b === void 0 ? '' : _b;
+        return (prefix + candidate).match(/[^aeiou]{4,}/) === null;
+    },
     no_consonant_pileup_1.default(2),
     consistent_glyphs_1.default(['kw', 'qu'], ['f', 'ph'], ['v', 'bh']),
-    reject_matches_1.default(/[dgkts]j/i, /[^aeiou][zj][^aeiou]/),
+    reject_matches_1.default(/[cdgktqz]j/i, // fewer difficult j sounds
+    /j[cdflrsyz]/i, // fewer difficult j sounds
+    /v[^aeiouy]/i, // fewer difficult v sounds
+    /[^aeiouy][zj][^aeiouy]/i, // fewer very difficult j/z sounds
+    /[gjktqx][cgjkqx]/i, // fewer difficult ckqg sounds
+    /[^aeiouy](gl|gn|gh)/i, // fewer difficult g sounds
+    /[^aeiouy]pn/i, // fewer difficult pn sounds
+    /([^aeiouy])[^aeiouy]\1/),
 ];
 //# sourceMappingURL=default-filters.js.map
--- src/default-filters.ts
+++ src/default-filters.ts
@@ -3,6 +3,10 @@ import consistentGlyphs from './filters/consistent-glyphs';
 import rejectMatches from './filters/reject-matches';
 
 export default [
+  (
+    candidate: string,
+    { prefix = '' }: { prefix: string},
+  ) => (prefix + candidate).match(/[^aeiou]{4,}/) === null,
   noConsonantPileup(2),
   consistentGlyphs(
     ['kw', 'qu'],
@@ -10,7 +14,13 @@ export default [
     ['v', 'bh'],
   ),
   rejectMatches(
-    /[dgkts]j/i,
-    /[^aeiou][zj][^aeiou]/,
+    /[cdgktqz]j/i, // fewer difficult j sounds
+    /j[cdflrsyz]/i, // fewer difficult j sounds
+    /v[^aeiouy]/i, // fewer difficult v sounds
+    /[^aeiouy][zj][^aeiouy]/i, // fewer very difficult j/z sounds
+    /[gjktqx][cgjkqx]/i, // fewer difficult ckqg sounds
+    /[^aeiouy](gl|gn|gh)/i, // fewer difficult g sounds
+    /[^aeiouy]pn/i, // fewer difficult pn sounds
+    /([^aeiouy])[^aeiouy]\1/, // fewer tripping sounds
   ),
 ];
