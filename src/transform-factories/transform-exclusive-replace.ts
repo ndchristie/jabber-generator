1b570360ccdf0a104b9f6447de257d8ba56259dd
Nathan Christie <ndchristie@gmail.com>
2018-01-31 17:10:22 -0500

    refactor: Add explicit array length check to replacement pair make the linter happy

--- src/transform-factories/transform-exclusive-replace.ts
+++ src/transform-factories/transform-exclusive-replace.ts
@@ -2,7 +2,7 @@ export default (...pairs): Function => (untransformed: string): string => {
   for (let i = 0; i < pairs.length; i += 1) {
     const match = untransformed.match(pairs[i][0]);
     if (match !== null && match.length > 0) {
-      return untransformed.replace(pairs[i][0], pairs[i][1] || '');
+      return untransformed.replace(pairs[i][0], pairs[i].length < 2 ? '' : pairs[i][1]);
     }
   }
   return untransformed;
