03e992740e68f883b5738e3301ac20861cee1f6f
Nathan Christie <ndchristie@gmail.com>
2018-01-31 17:02:21 -0500

    feat: Expose factories and exempt index from testing

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
 export { default } from './generator';
+
+export { default as filterInconsistentGraphemes }
+  from './filter-factories/filter-inconsistent-graphemes';
+export { default as filterConsonantPileups }
+  from './filter-factories/filter-consonant-pileups';
+export { default as filterMatches }
+  from './filter-factories/filter-matches';
+
+export { default as transformExclusiveReplace }
+  from './transform-factories/transform-exclusive-replace';
